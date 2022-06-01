81212ee1f23ca7343683631d256a66fed287fd6a
Anastasis Giannousakis <giannou@pik-potsdam.de>
2022-06-01 16:00:09 +0200

    adds possibility to include variable groups without the |+| notation to the summation checks

--- R/checkSummations.R
+++ R/checkSummations.R
@@ -13,11 +13,16 @@
 #'
 #' @return a list with the failed and/or missing variable groups as elements
 #' @importFrom mip extractVariableGroups
-#' @importFrom dplyr %>% filter group_by summarise select arrange
+#' @importFrom dplyr %>% filter group_by summarise select arrange mutate
 #' @export
 
 checkSummations <- function(x) {
-  sgroup <- extractVariableGroups(levels(x$variable), keepOrigNames = TRUE)
+
+  tmp <- read.csv(system.file("extdata", "summationGroups.csv", package = "remind2"), sep = ";", stringsAsFactors=FALSE)
+  sgroup <- NULL
+  for (i in unique(tmp[,"parent"])) sgroup[[i]] <- tmp[which(tmp[,"parent"]==i),"child"]
+
+  sgroup <- c(sgroup, extractVariableGroups(levels(x$variable), keepOrigNames = TRUE))
   failed <- NULL
   variable <- NULL
   region <- NULL
@@ -44,7 +49,12 @@ checkSummations <- function(x) {
       }
 
     }
-    ifelse(is.null(failed), message("Summations look good"), return(list("failing and/or missing variable groups", failed)))
+    if (is.null(failed)) {
+      message("Summations look good")
+    } else {
+      warning("Some variable groups do not sum up or the total is missing")
+      return(list("failing and/or missing variable groups", failed))
+    }
   } else {
     message("No summation groups found in the data")
   }
